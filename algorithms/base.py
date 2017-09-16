21042766f8624cdd938aca4859ddf2111850e9f6
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-09-16 14:07:07 -0700

    Allow graphs/samples to be loaded from variables in sampling/recovery

--- algorithms/base.py
+++ algorithms/base.py
@@ -1,33 +1,35 @@
 import json
 
-from utils import load_graph
-from utils import load_samples
+import networkx as nx
+
+from utils import load_graph, load_samples
 
 class GraphSamplingAlgorithm(object):
-  def __init__(self, graph_file):
+  def __init__(self, graph):
     super().__init__()
 
-    self.graph_file = graph_file
-    self.import_graph()
-
-  def import_graph(self):
-    self.graph = load_graph(self.graph_file)
+    if isinstance(graph, nx.Graph):
+      self.graph = graph
+    elif isinstance(graph, str):
+      self.graph = load_graph(graph)
 
   def run(self):
     raise NotImplementedError("{} must implement run method"
                               "".format(self.__class__.__name__))
 
 class GraphRecoveryAlgorithm(object):
-  def __init__(self, graph_file, sample_file):
+  def __init__(self, graph, samples):
     super().__init__()
 
-    self.graph_file = graph_file
-    self.import_graph()
-    self.sample_file = sample_file
-    self.import_samples()
+    if isinstance(graph, nx.Graph):
+      self.graph = graph
+    elif isinstance(graph, str):
+      self.graph = load_graph(graph)
 
-  def import_graph(self):
-    self.graph = load_graph(self.graph_file)
+    if isinstance(samples, dict):
+      self.samples = samples
+    elif isinstance(samples, str):
+      self.samples = load_samples(samples)
 
   def run(self):
     raise NotImplementedError("{} must implement run method"
