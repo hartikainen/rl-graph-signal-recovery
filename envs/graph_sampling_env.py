16ccd60a1a984b056bb97a881ea2d9f64a817227
Risto Vuorio <vuoristo@gmail.com>
2017-09-16 21:20:32 +0900

    Adds an env where the agent walks the graph
    * The environment stores current node and current edge in it's state
    * Actions are: sample node, advance to next edge, move along current edge
    * Reward is unfinished
    * Observation is currently just simple summary of state

---
+++ envs/graph_sampling_env.py
@@ -0,0 +1,97 @@
+"""Reinforcement learning environment presenting the graph sampling problem
+"""
+
+import logging
+import random
+
+from gym import Env
+from gym.spaces import Discrete, Tuple, Box
+from gym.utils import colorize, seeding
+import numpy as np
+import networkx as nx
+
+from algorithms.recovery import sparse_label_propagation
+from graph_functions import total_variance
+
+logger = logging.getLogger(__name__)
+
+class GraphSampling(Env):
+
+  metadata = {
+    'render.modes': ('human',),
+  }
+
+  def __init__(self, nx_graph, max_samples=10):
+    self.graph = nx.convert_node_labels_to_integers(nx_graph)
+    num_nodes = nx_graph.number_of_nodes()
+    self.sampling_set = set()
+
+    # actions: 0: sample 1: next edge 2: move
+    self.action_space = Discrete(3)
+
+    # observation: current node, number of edges from the current node, the
+    # currently selected edge, number of edges from the next node, ...
+    max_d = np.max(list(self.graph.degree().values()))
+    self.observation_space = Box(
+        np.array([0,0,0,0]),
+        np.array([self.graph.number_of_nodes(), max_d, max_d, max_d]))
+    self._current_node = 0
+    self._current_edge_idx = 0
+
+    self._seed()
+    self.reset()
+
+  def _seed(self, seed=None):
+    self.np_random, seed = seeding.np_random(seed)
+    return [seed]
+
+  def _reset(self):
+    self.sampling_set = set()
+    self._current_node = random.sample(self.graph.nodes(), 1)[0]
+    self._current_edge_idx = np.random.randint(
+        len(self.graph.neighbors(self._current_node)))
+    return self._get_observation()
+
+  def _validate_action(self, action):
+    if not self.action_space.contains(action):
+      raise ValueError("{} ({}) invalid".format(action, type(action)))
+
+  def _get_next_node(self):
+    neighbors = self.graph.neighbors(self._current_node)
+    return neighbors[self._current_edge_idx]
+
+  def _get_observation(self):
+    num_edges = self.graph.degree(self._current_node)
+    num_next_edges = self.graph.degree()[self._get_next_node()]
+    return np.array((self._current_node,
+                     num_edges,
+                     self._current_edge_idx,
+                     num_next_edges))
+
+  def _do_action(self, action):
+    # actions: 0: sample 1: next edge 2: move
+    if action == 0:
+      self.sampling_set.add(self._current_node)
+    elif action == 1:
+      self._current_edge_idx = ((self._current_edge_idx + 1)
+                                % self.graph.degree(self._current_node))
+    elif action == 2:
+      self._current_node = self._get_next_node()
+
+  def _step(self, action):
+    self._validate_action(action)
+    self._do_action(action)
+    observation = self._get_observation()
+
+    reward = 0.0
+    done = False
+
+    if action == 0 and len(self.sampling_set) > 0:
+      x_hat = sparse_label_propagation(self.graph, list(self.sampling_set))
+      lmbd = 1e-2 # TODO: make this parameter
+      reward = np.sum([
+        np.power(self.graph.node[i]['value'] - x_hat[i], 2.0)
+        for i in self.sampling_set
+      ]) + lmbd * total_variance(self.graph, x_hat)
+
+    return observation, reward, done, {}
--- test/test_env.py
+++ test/test_env.py
@@ -1,9 +1,16 @@
-from env import GraphSampling
-from utils import load_graph
+import networkx as nx
+
+from envs.graph_sampling_env import GraphSampling
+
+def create_graph():
+  g = nx.random_partition_graph([5,5], 1.0, 1.0)
+  for node_index in g:
+    g.node[node_index]['value'] = 1.0
+
+  return g
 
 def test_env():
-  g = load_graph("./data/graphs/out2.json")
+  g = create_graph()
   env = GraphSampling(g)
-
-  for action in range(5):
-    env.step(action)
+  for i in range(10):
+    env.step(i % 3)
