ff71867a82fe324e3e40290023a6cb198a8487ff
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2017-10-06 16:36:21 -0700

    Update BaseAgent

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow.contrib.layers as layers
 from gym.spaces import Box, MultiBinary, Discrete
-from baselines import deepq
+from baselines import deepq, logger
 from baselines.common import tf_util
 from baselines.common.schedules import LinearSchedule
 from baselines.deepq.replay_buffer import ReplayBuffer
@@ -10,37 +11,51 @@ from datetime import datetime
 from algorithms.recovery import sparse_label_propagation
 from graph_functions import nmse
 from utils import TIMESTAMP_FORMAT, dump_pickle
+from visualization import draw_partitioned_graph
+
+def model_fn(inpt, num_actions, scope, reuse=False):
+    """This model takes as input an observation and returns values of all actions."""
+    with tf.variable_scope(scope, reuse=reuse):
+        out = inpt
+        out = layers.fully_connected(out, num_outputs=64, activation_fn=tf.nn.tanh)
+        out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
+        return out
 
 class BaseAgent(object):
   def __init__(self, env):
     self.env = env
     self._build_train()
-    self.session = tf_util.make_session(1)
+    self.session = tf_util.make_session(8)
+
+  def _observation_ph_generator(self, name):
+    env = self.env
+
+    if isinstance(env.observation_space, (MultiBinary, Discrete)):
+      batch_shape = (env.observation_space.n,)
+    elif isinstance(env.observation_space, Box):
+      batch_shape = env.observation_space.shape
+    else:
+      raise ValueError("Unexpected observation space")
+
+    return tf_util.BatchInput(batch_shape, name=name)
 
   def _build_train(self):
     env = self.env
 
-    def observation_ph_generator(name):
-      if isinstance(env.observation_space, (MultiBinary, Discrete)):
-        batch_shape = (env.observation_space.n,)
-      elif isinstance(env.observation_space, Box):
-        batch_shape = env.observation_space.shape
-      else:
-        raise ValueError("Unexpected observation space")
-      return tf_util.BatchInput(batch_shape, name=name)
-
     act, train, update_target, debug = deepq.build_train(
-      make_obs_ph=observation_ph_generator,
-      q_func=deepq.models.mlp([100]),
+      make_obs_ph=self._observation_ph_generator,
+      q_func=model_fn,
       num_actions=env.action_space.n,
-      optimizer=tf.train.AdamOptimizer(learning_rate=1),
+      optimizer=tf.train.AdamOptimizer(learning_rate=5e-4),
+      gamma=0.99
     )
+
     self.act = act
     self.train = train
     self.update_target = update_target
     self.debug = debug
 
-  def learn(self, num_train_graphs=100):
+  def learn(self, num_train_graphs=10000):
     env = self.env
 
     act = self.act
@@ -51,10 +66,10 @@ class BaseAgent(object):
 
     with self.session.as_default():
       # Create the replay buffer
-      replay_buffer = ReplayBuffer(32)
-      # Create the schedule for exploration starting from 1 (every action is random) down to
-      # 0.02 (98% of actions are selected according to values predicted by the model).
-      exploration = LinearSchedule(schedule_timesteps=1000,
+      replay_buffer = ReplayBuffer(50000)
+      # Create the schedule for exploration starting from 1 (every action is
+      # random) down to 0
+      exploration = LinearSchedule(schedule_timesteps=10000,
                                    initial_p=1.0,
                                    final_p=0.02)
 
@@ -82,11 +97,15 @@ class BaseAgent(object):
           if done:
             nmse = env.get_current_nmse()
             if len(episode_rewards) % 10 == 0:
-              print("steps", t)
-              print("episodes", len(episode_rewards))
-              print("mean episode reward", round(np.mean(episode_rewards[-101:-1]), 1))
-              print("nmse: ", nmse)
-              print("% time spent exploring", int(100 * exploration.value(t)))
+              logger.record_tabular("steps", t)
+              logger.record_tabular("episodes", len(episode_rewards))
+              logger.record_tabular("mean episode reward",
+                                    round(np.mean(episode_rewards[-10:-1]), 1))
+              logger.record_tabular("nmse", nmse)
+              logger.record_tabular("sampling set", list(env.sampling_set))
+              logger.record_tabular("% time spent exploring",
+                                    int(100 * exploration.value(t)))
+              logger.dump_tabular()
 
             observation = env.reset()
 
@@ -105,18 +124,14 @@ class BaseAgent(object):
             env.render()
           else:
             # Minimize the Bellman equation error on replay buffer sample batch
-            if t > 10:
+            if t > 1000:
               (observations_t, actions, rewards,
                observations_tp1, dones) = replay_buffer.sample(32)
               train(observations_t, actions, rewards,
                     observations_tp1, dones, np.ones_like(rewards))
-            if t % 10 == 0:
+            if t % 1000 == 0:
               # Update target network periodically.
               update_target()
-
-    now = datetime.now()
-    results_path = f"./results/fixed_env/{now.strftime(TIMESTAMP_FORMAT)}.pk"
-    dump_pickle(results, results_path)
 
   def test(self):
     env = self.env
