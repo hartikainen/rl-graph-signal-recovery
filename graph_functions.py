2c43b15f2b835b78be79dc7bd44ea1637c8c68e4
Risto Vuorio <vuoristo@gmail.com>
2017-10-14 15:16:22 +0900

    Adds graph function for finding minimum slp error for small graphs

--- graph_functions.py
+++ graph_functions.py
@@ -1,4 +1,9 @@
+from itertools import combinations
+
 import numpy as np
+
+from algorithms.recovery import sparse_label_propagation
+
 
 def total_variation(edges, signal):
   tv = np.sum(np.absolute([
@@ -31,5 +36,21 @@ def slp_maximum_error(x):
     maximally_different_x[index] = x[np.argmax(np.abs(x - element_vector))]
   error = normalized_mean_squared_error(x, maximally_different_x)
   return error
+
+def slp_minimum_error(graph, sampling_set_size):
+  """Finds the minimum error for given graph and sampling set size.
+
+  Very slow for large graphs due to exhaustive search.
+  """
+  sampling_sets = combinations(graph.nodes(), sampling_set_size)
+  x = [graph.node[idx]['value']
+       for idx in range(graph.number_of_nodes())]
+  errors = []
+  for sampling_set in sampling_sets:
+    x_hat = sparse_label_propagation(graph, sampling_set)
+    error = normalized_mean_squared_error(x, x_hat)
+    errors.append(error)
+  return min(errors)
+
 
 nmse = normalized_mean_squared_error
